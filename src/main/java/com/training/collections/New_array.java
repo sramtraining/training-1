e8889d51fad4a6c12eb8cfc664d2a6214a2eb7d0
SURYA TEJA LAKSHMISETTY <suryatejalakshmisetty@suryas-mbp.myfiosgateway.com>
2022-02-09 23:32:08 -0500

    added New array

---
+++ src/main/java/com/training/collections/New_array.java
@@ -0,0 +1,9 @@
+package com.training.collections;
+
+public class New_array {
+    public static void main(String[] args) {
+        int[] intArr = {55,66,88,98,99};
+                for (int i : intArr)
+                    System.out.println(i);
+    }
+}
