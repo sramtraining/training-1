784cc58b5915688aceabf277e6ac6d49670e7c57
sethuramans <sethuraman.rit@gmail.com>
2022-02-10 07:46:36 +0530

    Collections Code Base added

---
+++ src/main/java/com/training/collections/Arrays.java
@@ -0,0 +1,12 @@
+package com.training.collections;
+
+public class Arrays {
+
+
+    public static void main(String[] args) {
+        int[] intArr = {1, 2, 3, 4, 5};
+        for (int i : intArr) {
+            System.out.println(i);
+        }
+    }
+}
